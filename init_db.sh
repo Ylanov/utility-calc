2ac7c5fc725843fc541cfdf7daa5790418086edf
ylanov <yroshuk.2001@gmail.com>
2026-03-12 10:23:45 +0300

    Nuclear option: Force Postgres to use /tmp for sockets_17

--- init_db.sh
+++ init_db.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    SELECT 'CREATE DATABASE arsenal_db'
-    WHERE NOT EXISTS (
-        SELECT FROM pg_database WHERE datname = 'arsenal_db'
-    )\gexec
+echo "Checking required databases..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
+
+SELECT 'CREATE DATABASE utility_db'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'utility_db'
+)\gexec
+
+SELECT 'CREATE DATABASE arsenal_db'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'arsenal_db'
+)\gexec
+
 EOSQL
 
-echo "Database 'arsenal_db' checked/created."
+echo "Databases utility_db and arsenal_db are ready."
